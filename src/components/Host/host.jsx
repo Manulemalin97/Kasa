202da577223746af5ad563b0563d8752b161f141
unknown <m.jansen@outlook.fr>
2023-03-30 16:49:32 +0200

    Host mis a jour, responsive fait

--- src/components/Host/host.jsx
+++ src/components/Host/host.jsx
@@ -8,22 +8,22 @@
  *
  **/
 
-import React from 'react'
+import React from 'react' // importation du module reavt
 import './host.css' // importe le fichier de styles "host.css"
 
-// déclare un composant "Host" qui prend deux propriétés : "name" et "picture"
+// déclare un composant "Host" qui prend deux propriétés, name et picture
 
 const Host = ({ name, picture }) => {
    // retourne le JSX du composant
    return (
       // div englobant tout le contenu du composant avec la classe "host-container"
       <div className="host-container">
-         {/* div pour l'image de l'hôte avec la classe "host-image-container" */}
+         {/* div pour l'image de l'hote avec la classe "host-image-container" */}
          <div className="host-image-container">
-            {/*  balise img pour afficher l'image de l'hôte avec la classe "host-image" */}
+            {/*  balise img pour afficher l'image de l'hote avec la classe "host-image" */}
             <img className="host-image" src={picture} alt={name} />
          </div>
-         {/* div pour afficher le nom de l'hôte avec la classe "host-name" */}
+         {/* div pour afficher le nom de l'hote avec la classe "host-name" */}
          <div className="host-name">{name}</div>
       </div>
    )
